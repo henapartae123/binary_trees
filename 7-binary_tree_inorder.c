8641e96eba4cff309f95c6a3982d74e22aaa291a
henapaartae123 <henyvibe@gmail.com>
2022-08-26 15:50:12 +0300

    trees

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -15,6 +15,5 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 		binary_tree_inorder(tree->left, func);
 		binary_tree_inorder(tree->right, func);
         func(tree->n);
-        
 	}
 }
