307a028d10ede7bafef58f18c0f4e1a92ec3625a
unknown <henyvibe@gmail.com>
2022-12-11 12:12:23 +0300

    balance

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,13 +8,41 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-        size_t nodes = 0;
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
 
-        if (tree)
-        {
-                nodes += (tree->left || tree->right) ? 1 : 0;
-                nodes += binary_tree_nodes(tree->left);
-                nodes += binary_tree_nodes(tree->right);
-        }
-        return (nodes);
+	return (binary_tree_size(tree) - binary_tree_leaves(tree));
+}
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: tree to measure the size of
+ *
+ * Return: size of the tree
+ *         0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: tree to count the leaves from
+ *
+ * Return: number of leaves
+ *         0 if tree is NULL
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
