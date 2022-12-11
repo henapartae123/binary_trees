604c20446101421aa767f5fc254e47272aea0478
unknown <henyvibe@gmail.com>
2022-12-11 12:42:09 +0300

    balance

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - functin that measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: A pointer to the root node of the tree to measure the height
  * 
- * Return: height
+ * Return: height of the tree
  *         0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
@@ -18,6 +18,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
         return ((left > right) ? left : right);
     
     }
-
     return (0);
 }
