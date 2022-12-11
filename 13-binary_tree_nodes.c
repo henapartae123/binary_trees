791256265c797d0af178e5d72c4eb5b6658d99cc
unknown <henyvibe@gmail.com>
2022-12-11 11:54:32 +0300

    balance

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,11 +1,10 @@
 #include "binary_trees.h"
 /**
- * binary_tree_nodes - functino that counts the nodes at laest 1 child in a binary tree
- * @tree: a pointer to the root node of the tree to count the number of nodes
+ * binary_tree_nodes - Counts the nodes at laest 1 child in a binary tree
+ * @tree: A pointer to the root node of the tree to count the number of nodes
  *
- * Return: number of nodes
- *         0 if tree is NULL
- *         NUll if not a leaf
+ * Return: Number of nodes,if tree is NULL, 0, if not a leaf, NUll. 
+ *         
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
@@ -17,6 +16,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
         nodes += binary_tree_nodes(tree->left);
         nodes += binary_tree_nodes(tree->right);
     }
-
     return (nodes);
 }
