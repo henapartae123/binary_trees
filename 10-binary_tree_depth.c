e47eb96a947f34d6cef85045b8eb81016b139cfd
unknown <henyvibe@gmail.com>
2022-12-11 12:36:39 +0300

    balance

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,5 +8,16 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+	size_t depth = 0;
+
+	if (!tree)
+		return (0);
+
+	while (tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+
+	return (depth);
 }
