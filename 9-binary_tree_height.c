ac1f84b73854e89dfb50861a37e01c6be7e0ca0e
unknown <henyvibe@gmail.com>
2022-12-11 12:42:26 +0300

    balance

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,7 +16,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
         left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
         right = tree->left ? 1 + binary_tree_height(tree->right) : 0;
         return ((left > right) ? left : right);
-    
     }
     return (0);
 }
