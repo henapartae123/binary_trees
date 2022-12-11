9e06ba94da38031233acd25b9600d7eada0cd0a2
unknown <henyvibe@gmail.com>
2022-12-11 12:24:43 +0300

    balance

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,10 +10,10 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 		if (!tree)
-			return (0);
+				return (0);
 
 		if (!tree->left && !tree->right)
-			return (1);
+				return (1);
 
 		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
