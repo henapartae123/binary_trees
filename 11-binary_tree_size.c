32f82940bb13089a1a5a0ce08237d6d914e5a96b
unknown <henyvibe@gmail.com>
2022-12-11 12:29:02 +0300

    balance

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -4,20 +4,13 @@
  * binary_trees_size - Measures the size of a binary tree
  * @tree: A pointer to the root node of the tree to measure the size
  * 
- * Return: size
- *         0 if tree is NULL
+ * Return: Size, If tree is NULL, 0.
+ *         
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t size = 0;
+	if (!tree)
+		return (0);
 
-    if (tree)
-    {
-        size += 1;
-        size += binary_tree_size(tree->left);
-        size += binary_tree_size(tree->right);
-
-    }
-    return(size);
-
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
