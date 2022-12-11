f5142ef416ee37c7ec71d0dee0854ceba279968c
unknown <henyvibe@gmail.com>
2022-12-11 14:44:29 +0300

    adv

--- binary_trees.h
+++ binary_trees.h
@@ -22,6 +22,8 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
+void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level);
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
