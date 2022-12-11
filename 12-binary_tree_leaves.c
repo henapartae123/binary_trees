0fb5152d77e324b1385459b0d3a996455fc5e015
unknown <henyvibe@gmail.com>
2022-12-11 12:15:58 +0300

    balance

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_leaves - function that counts the leavesa= in a binary tree
- * @tree: a pointer to the root node of the tree to count the number of leaves
+ * binary_trees_leaves - Counts the leavesa= in a binary tree
+ * @tree: A pointer to the root node of the tree to count the number of leaves
  * 
  * Return: number of leaves
  *         0 if tree is NULL
@@ -10,14 +10,11 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t leaves = 0;
+    if (!tree)
+		return (0);
 
-	if (tree)
-	{
-		leaves += (!tree->left && !tree->right) ? 1 : 0;
-		leaves += binary_tree_leaves(tree->left);
-		leaves += binary_tree_leaves(tree->right);
-	}
-    
-	return (leaves);
+	if (!tree->left && !tree->right)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
