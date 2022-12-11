515bd1f7fd4185bfbd0e274fde1412490aef4c27
unknown <henyvibe@gmail.com>
2022-12-11 12:46:27 +0300

    balance

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,18 +5,18 @@
  * @tree: A pointer to the root node of the tree to measure the height.
  *
  * Return: If tree is NULL - 0.
- *         Otherwise -height of the tree.
+ *         Otherwise - height of the tree.
  *
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (tree)
-    {
-        size_t left = 0, right = 0;
+	size_t height_l = 0;
+	size_t height_r = 0;
 
-        left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-        right = tree->left ? 1 + binary_tree_height(tree->right) : 0;
-        return ((left > right) ? left : right);
-    }
-    return (0);
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
